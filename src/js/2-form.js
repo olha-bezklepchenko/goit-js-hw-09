d572a6d5d0c5a18ac947c9f1c8ca08cbfe5d958a
Olha Bezklepchenko <krolichek.11@gmail.com>
2024-07-12 11:27:51 +0200

    add comments

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,11 +1,18 @@
+// Oголошуємо об'єкт formData
+
 let formData = { email: '', message: '' };
 
+// Отримуємо посилання на форму
 const form = document.querySelector('.feedback-form');
 
+// Додаємо обробників подій input і submit до форми
 form.addEventListener('input', handlerInput);
 form.addEventListener('submit', handlerSubmit);
+
+// Викликаємо функцію для заповнення форми при завантаженні сторінки
 populateForm();
 
+// Функція для обробки події input
 function handlerInput(event) {
   const value = event.target.value.trim();
   const key = event.target.name;
@@ -15,8 +22,10 @@ function handlerInput(event) {
     localStorage.setItem('feedback-form-state', JSON.stringify(formData));
   } catch (err) {
     console.error(err);
+    return;
   }
 }
+// Функція, яка перевіріє, чи є дані у локальному сховищі.Якщо є, використовує їх для заповнення форми та об'єкта formData.
 
 function populateForm() {
   try {
@@ -35,6 +44,7 @@ function populateForm() {
   }
 }
 
+//Функція для обробки події submit
 function handlerSubmit(event) {
   event.preventDefault();
 
